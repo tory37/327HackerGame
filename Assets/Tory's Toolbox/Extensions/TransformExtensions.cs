7c69be33d6969de93a8d96fb48491cb7b87ab5e6
tory37 <tory37@gmail.com>
2015-10-26 22:22:28 -0500

    added my toolbox and visual studio package

---
+++ Assets/Tory's Toolbox/Extensions/TransformExtensions.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Diagnostics;
+
+public static class TransformExtensions {
+
+	public static void SetPositionX(this Transform t, float newX)
+    {
+        t.position = new Vector3(newX, t.position.y, t.position.z);
+    }
+
+    public static void SetPositionY(this Transform t, float newY)
+    {
+        t.position = new Vector3(t.position.x, newY, t.position.z);
+    }
+
+    public static void SetPositionZ(this Transform t, float newZ)
+    {
+        t.position = new Vector3(t.position.x, t.position.y, newZ);
+    }
+}
