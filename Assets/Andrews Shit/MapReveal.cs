eb5cb74bb4e10cf44c327e7127efc608daa9262b
Adurnis <andyru12@yahoo.com>
2015-11-20 02:41:53 -0800

    Map revelation is done.
    A mini-map on the bottom right of the screen shows the parts of the maze
    the player has visited by "logging" it when they arrive in a cell.

---
+++ Assets/Andrews Shit/MapReveal.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MapReveal : MonoBehaviour {
+
+    MazeGenerator MG;
+
+    public void OnTriggerEnter(Collider other)
+    { 
+        //GameObject ceiling = (GameObject)other.gameObject;
+        
+        if(other.tag == "ceiling")
+        {
+            Debug.Log(other.gameObject.ToString());
+            other.gameObject.layer = 8;
+            foreach(Transform child in other.transform)
+            {
+                child.gameObject.layer = 8;
+            }
+            
+            //Debug.Log("Entered a new floor panel");
+            //if (MG.ceilingPieces.Contains(ceiling))
+           // {
+            //    MG.ceilingPieces.Remove(ceiling);
+            //    ceiling.layer = 8;
+           // }
+        }
+    }
+}
