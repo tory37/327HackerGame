d5b2af30f84313854538c176ba0b2ab239e39537
tory37 <tory37@gmail.com>
2015-12-08 13:42:22 -0600

    Fixed fsm dirty

--- Assets/Scripts/UIManager.cs
+++ Assets/Scripts/UIManager.cs
@@ -1,15 +1,18 @@
 ﻿using UnityEngine;
 using System.Collections;
+using UnityEngine.UI;
 
 public class UIManager : MonoBehaviour {
 
-	// Use this for initialization
-	void Start () {
-	
+	void Awake()
+	{
+		DontDestroyOnLoad( transform.gameObject );
 	}
-	
-	// Update is called once per frame
-	void Update () {
-	
+
+	public void Transition(string sceneName)
+	{
+		Application.LoadLevel( sceneName );
 	}
+
+
 }
