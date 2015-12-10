3a1c4a3853a0674a77a05e4040961b37574cff7a
tory37 <tory37@gmail.com>
2015-12-10 17:10:09 -0600

    Powerups and gui

--- Assets/Scripts/PlayerManager.cs
+++ Assets/Scripts/PlayerManager.cs
@@ -12,23 +12,26 @@ public class PlayerManager : MonoBehaviour {
     {
         powerUpDict.Add(PowerUpTypes.Invisibility, 0);
         powerUpDict.Add(PowerUpTypes.Stun, 0);
+
+		GameSpecificGUI.Instance.CyclePUP( currentPowerUp, powerUpDict[currentPowerUp] );
     }
-	
+
 	// Update is called once per frame
 	void Update () 
     {
 	    //logic for choosing which powerup to use and when to use it here
-        if (Input.GetKeyDown("e"))
+        if (Input.GetKeyDown(KeyCode.E))
         {
             if (powerUpDict[currentPowerUp] > 0)
             {
                 //use the current poewrup 
                 PowerUpManager.Instance.DoPowerUp(currentPowerUp);
                 powerUpDict[currentPowerUp]--;
+				GameSpecificGUI.Instance.UpdatePUPCount( powerUpDict[currentPowerUp] );
             }
         }
 
-        if (Input.GetKeyDown("q"))
+        if (Input.GetKeyDown(KeyCode.Q))
         {
             Debug.Log("Changing the powerup type");
             //change the powerup to be the other one
@@ -36,21 +39,23 @@ public class PlayerManager : MonoBehaviour {
                 currentPowerUp = PowerUpTypes.Invisibility;
             else
                 currentPowerUp = PowerUpTypes.Stun;
+
+			GameSpecificGUI.Instance.CyclePUP( currentPowerUp, powerUpDict[currentPowerUp] );
         }
 	}
 
-    void OnTriggerEnter(Collider other)
+    void OnCollisionEnter(Collision col)
     {
-        if (other.tag == "PowerUp")
+        if (col.collider.tag == "PowerUp")
         {
             //the the power up script component from the collided object to know what the type the power up is 
-            PowerUP pu = other.GetComponent<PowerUP>();
+            PowerUP pu = col.collider.GetComponent<PowerUP>();
             if (pu != null)
             {
                 powerUpDict[pu.Type]++;
             }
             //destroy the power up so that the player can only receive it once. 
-            GameObject.Destroy(other.gameObject);
+            GameObject.Destroy(col.gameObject);
         }
     }
 }
